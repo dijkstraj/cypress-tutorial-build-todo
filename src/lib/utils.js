84a090b732af12507cb008ef3f5d99eb34717e16
Jogchem Dijkstra <jogchem@gmail.com>
2020-09-27 23:17:15 +0200

    Implement application changes

---
+++ src/lib/utils.js
@@ -0,0 +1,4 @@
+export const filterTodos = (filter, todos) => todos
+// export const filterTodos = (filter, todos) => filter
+//   ? todos.filter(todo => todo.isComplete === (filter === 'completed'))
+//   : todos
